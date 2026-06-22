1dd3ba875231960f0d6af211cee35982c7a61352
Musca Mihail <muscamihaill@gmail.com>
2026-06-22 15:52:19 +0300

    fix: scripts venv dir paths

--- scripts/files.py
+++ scripts/files.py
@@ -1,10 +1,12 @@
 from pathlib import Path
+import sys
 
 import __about__ as a
 
 PROJECT_ROOT = Path(a.__file__).parent
-VENV_LIB = PROJECT_ROOT / ".venv" / "lib"
-VENV_LIB_SITEPACKAGES = next(p for p in Path(VENV_LIB).iterdir() if p.is_dir()) / "site-packages"
+VENV_LIB = PROJECT_ROOT / ".venv" / ("Lib" if sys.platform == "win32" else "lib")
+_VENV_LIB_PYTHON = VENV_LIB if (VENV_LIB / "site-packages").is_dir() else next(p for p in Path(VENV_LIB).iterdir() if p.is_dir())
+VENV_LIB_SITEPACKAGES = _VENV_LIB_PYTHON / "site-packages"
 SRC = PROJECT_ROOT / "src"
 SRC_QUILL_HOME = SRC / "_"
 SRC_QUILL_PACKAGE = SRC

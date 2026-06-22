f563f21aaed9a08e570650520672bde13c34c2c4
Musca Mihail <muscamihaill@gmail.com>
2026-06-22 11:25:03 +0300

    feat: update build script to include python dependencies, update quill.json & github build workflow to use uv run build, added jpype dependency for testing

--- scripts/files.py
+++ scripts/files.py
@@ -8,3 +8,5 @@ SRC_QUILL_HOME = SRC / "_"
 SRC_QUILL_PACKAGE = SRC
 BUILD = PROJECT_ROOT / "build"
 BUILD_GENERATED = BUILD / "generated"
+VENV_LIB = PROJECT_ROOT / ".venv" / "lib"
+VENV_LIB_SITEPACKAGES = next(p for p in Path(VENV_LIB).iterdir() if p.is_dir()) / "site-packages"
--- scripts/run/build.py
+++ scripts/run/build.py
@@ -1,7 +1,7 @@
 import zipfile
 
 import __about__ as a
-from scripts.files import SRC, BUILD, BUILD_GENERATED
+from scripts.files import SRC, BUILD, BUILD_GENERATED, VENV_LIB_SITEPACKAGES
 from scripts import generate
 
 def run():
@@ -16,3 +16,15 @@ def run():
         for f in BUILD_GENERATED.rglob("*"):
             if f.is_file():
                 zip.write(f, f.relative_to(BUILD_GENERATED))
+        for lib in VENV_LIB_SITEPACKAGES.iterdir():
+            if lib.is_dir():
+                if lib.name == "__pycache__" or lib.name.endswith(".dist-info"):
+                    continue
+                for f in lib.rglob("*"):
+                    if f.is_file():
+                        zip.write(f, f.relative_to(VENV_LIB_SITEPACKAGES))
+            else:
+                if lib.name == "_virtualenv.py" or lib.name.endswith(".pth"):
+                    continue
+                zip.write(lib, lib.relative_to(VENV_LIB_SITEPACKAGES))
+            

669772fbe7539d3d8c3a4142970accca594e4edd
Musca Mihail <muscamihaill@gmail.com>
2026-06-22 13:11:42 +0300

    refactor: reorder files & build order

--- scripts/files.py
+++ scripts/files.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
-import scripts
+import __about__ as a
 
-PROJECT_ROOT = Path(scripts.__file__).parent.parent
+PROJECT_ROOT = Path(a.__file__).parent
+VENV_LIB = PROJECT_ROOT / ".venv" / "lib"
+VENV_LIB_SITEPACKAGES = next(p for p in Path(VENV_LIB).iterdir() if p.is_dir()) / "site-packages"
 SRC = PROJECT_ROOT / "src"
 SRC_QUILL_HOME = SRC / "_"
 SRC_QUILL_PACKAGE = SRC
 BUILD = PROJECT_ROOT / "build"
 BUILD_GENERATED = BUILD / "generated"
-VENV_LIB = PROJECT_ROOT / ".venv" / "lib"
-VENV_LIB_SITEPACKAGES = next(p for p in Path(VENV_LIB).iterdir() if p.is_dir()) / "site-packages"
--- scripts/run/build.py
+++ scripts/run/build.py
@@ -1,21 +1,15 @@
 import zipfile
 
 import __about__ as a
-from scripts.files import SRC, BUILD, BUILD_GENERATED, VENV_LIB_SITEPACKAGES
+from scripts.files import VENV_LIB_SITEPACKAGES, SRC, BUILD, BUILD_GENERATED
 from scripts import generate
 
 def run():
-    generate()
-
     BUILD.mkdir(parents=True, exist_ok=True)
 
+    generate()
+
     with zipfile.ZipFile(BUILD / f"{a.ID}-bundle.zip", "w", zipfile.ZIP_DEFLATED) as zip:
-        for f in SRC.rglob("*"):
-            if f.is_file():
-                zip.write(f, f.relative_to(SRC))
-        for f in BUILD_GENERATED.rglob("*"):
-            if f.is_file():
-                zip.write(f, f.relative_to(BUILD_GENERATED))
         for lib in VENV_LIB_SITEPACKAGES.iterdir():
             if lib.is_dir():
                 if lib.name == "__pycache__" or lib.name.endswith(".dist-info"):
@@ -27,4 +21,10 @@ def run():
                 if lib.name == "_virtualenv.py" or lib.name.endswith(".pth"):
                     continue
                 zip.write(lib, lib.relative_to(VENV_LIB_SITEPACKAGES))
+        for f in SRC.rglob("*"):
+            if f.is_file():
+                zip.write(f, f.relative_to(SRC))
+        for f in BUILD_GENERATED.rglob("*"):
+            if f.is_file():
+                zip.write(f, f.relative_to(BUILD_GENERATED))
             
